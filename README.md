e48f6f6a8bfa3c0d1d301d30d4e1c939b905b773
panqingyi <panqingyi@cloudin.cn>
2019-01-08 17:26:02 +0800

    feat: add ci status.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-## teof-blog
+# Teof-blog [![CircleCI](https://circleci.com/gh/teofpqy/teof-blog.svg?style=svg)](https://circleci.com/gh/teofpqy/teof-blog)
 
 [view url](https://teofpqy.github.io/teof-blog/)
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello =() => <h1>Hello, This is teof's blog.</h1>;
+const Hello =() => <div>
+  <h1>Hello, This is teof's blog.</h1>
+  <a href="https://github.com/teofpqy/teof-blog">git url</a>
+  </div>;
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
